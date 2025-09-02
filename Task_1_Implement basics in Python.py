6eae9d68026cfcd132b9a579946f456edbccb22e
premsailinga <premsailinga75@gmail.com>
2025-09-02 11:44:07 +0530

    Add files via upload

---
+++ Task_1_Implement basics in Python.py
@@ -0,0 +1,85 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x =int(input("Enter the First number:"))
+Enter the First number:5
+y =int(input("Enter the Second number:"))
+Enter the Second number:6
+add = x+y
+sub = x-y
+pro = x*y
+div = x/y
+
+print("Addition:",add)
+Addition: 11
+print("Subtraction :",sub)
+Subtraction : -1
+print("Multiplication:",pro)
+Multiplication: 30
+print("Division:",div)
+Division: 0.8333333333333334
+
+
+
+
+
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #Initializing the value of a,b,and c
+>>> a =int(input("Enter the first number:"))
+Enter the first number:5
+>>> b =int(input("Enter the Second number:"))
+Enter the Second number:6
+>>> c =int("Enter the Third number:"))
+SyntaxError: unmatched ')'
+>>> c =int(input("Enter the third number:"))
+Enter the third number:7
+>>> #Using relational operators
+>>> print(a,">",b,"is",a>b)
+5 > 6 is False
+>>> print(a,"<",b,"is",a<b)
+5 < 6 is True
+>>> print(c,"==",a,"is",c == a)
+7 == 5 is False
+>>> print(c,"!=",b,"is",c !=b)
+7 != 6 is True
+>>> print(a,">=","is",a>=b)
+5 >= is False
+>>> print(b,"<=",a,"is",b<=a)
+6 <= 5 is False
+>>> 6 <= 5 is False
+False
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #Taking three numbers as input
+>>> a=int(input("Enter the First number:"))
+Enter the First number:5
+>>> b=int(input("Enter the Second number:"))
+Enter the Second number:6
+>>> c=int(input("Enter the Third number:"))
+Enter the Third number:7
+>>> #Performing logical operations
+>>> print("\nLogical Operations Results:")
+
+Logical Operations Results:
+>>> print((a>b)and(b>c))
+False
+>>> print((a>b)or(b>c))
+False
+>>> print(not(a>b))
+True
+>>> print(not(b>c))
+True
