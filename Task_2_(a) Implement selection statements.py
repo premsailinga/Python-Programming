524048578bde977b86ea1312be2b051239dab184
premsailinga <premsailinga75@gmail.com>
2025-09-02 11:49:06 +0530

    Add files via upload

---
+++ Task_2_(a) Implement selection statements.py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> score =int(input("Enter the score:"))
+Enter the score:60
+>>> if score>=90:
+...     print("The Grade is A")
+... elif (score <= 89 and score >= 80):
+...     print("The Grade is B")
+... elif (score <= 79 and score >= 70):
+...     print("The Grade is C")
+... elif (score <= 69 and score >= 60):
+...     print("The Grade is D")
+... else:
+...     print (" the grade is F")
+... 
+...     
+The Grade is D
---
+++ Task_2_(b) Implement looping statements.py
@@ -0,0 +1,20 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Displaying the first 10 natural numbers
+>>> print("The first 10 natural numbers are:")
+The first 10 natural numbers are:
+>>> for i in range(1,11):#Loop from 1 to 10
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
